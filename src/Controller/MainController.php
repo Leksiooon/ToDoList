674e70962702e22503fa7e251f11fade0afe85a7
Leksiooon <sierocinski.maciej@o2.pl>
2019-08-24 09:11:14 +0200

    logging

---
+++ src/Controller/MainController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main")
+     */
+    public function index()
+    {
+        return $this->render('main.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $helper)
+    {
+
+        return $this->render('security/login.html.twig', [
+            'error' => $helper->getLastAuthenticationError()
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(): void
+    {
+        throw new \Exception('This should never be reached!');
+    }
+
+    /**
+     * @Route("/registration", name="registration")
+     */
+    public function registration()
+    {
+        return $this->render('security/registration.html.twig');
+    }
+}
