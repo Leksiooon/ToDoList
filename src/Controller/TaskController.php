eba50057afbe949167b3d776344482c37d36f8fa
Leksiooon <sierocinski.maciej@o2.pl>
2019-09-11 16:22:51 +0200

    moving task (left, right)

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Status;
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +12,47 @@ class TaskController extends AbstractController
     /**
      * @Route("/task/delete/{id}", name="deleteTask")
      */
-    public function index()
+    public function index(int $id)
     {
-        return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
-        ]);
+        $em = $this->getDoctrine()->getManager();
+
+        $tmpObj = $em->getRepository(Task::class)->find($id);
+        $tmpToDoList = $tmpObj->getToDoList();
+
+        $em->remove($tmpObj);
+        $em->flush();
+
+        return $this->redirectToRoute('toDoList', ['id' => $tmpToDoList->getId()]);
     }
+
+   /**
+    * @Route("/task/move/{id}/{move}", name="move")
+    */
+   public function move(int $id, string $move)
+   {
+       $em = $this->getDoctrine()->getManager();
+
+       $task = $em->getRepository(Task::class)->find($id);
+       $toDoList = $task->getToDoList();
+       $status = $task->getStatus();
+
+       $newPosition = 0;
+
+       if($move == 'right')
+           $newPosition = 1;
+       if($move == 'left')
+           $newPosition = -1;
+
+       $newStatus = $em->getRepository(Status::class)->findOneBy([
+           'toDoLists' => $toDoList,
+           'position' => $status->getPosition() + $newPosition
+       ]);
+
+       $task->setStatus($newStatus);
+
+       $em->persist($task);
+       $em->flush();
+
+       return $this->redirectToRoute('toDoList', ['id' => $toDoList->getId()]);
+   }
 }
